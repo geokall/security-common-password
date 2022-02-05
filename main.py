425c277130850d5c50470880e831c36ac3230091
geokall <gkallimanis@iknowhow.com>
2022-02-05 23:21:02 +0200

    latest updates

--- main.py
+++ main.py
@@ -1,73 +1,53 @@
-import nacl.pwhash
-import nacl.hash
+import argon2
 from argon2 import PasswordHasher
 
 
-# def test_hashing():
-#
-# password = b'stronk-password'
-# hashed = nacl.pwhash.str(password)
-# print(hashed)
-#
-# wrong_password = b'qwerty123'
-#
-# try:
-#     res = nacl.pwhash.verify(hashed, wrong_password)
-#     print(res)
-# except:
-#     print('false')
-
-# def hash_examples(password):
-#     ph = PasswordHasher()
-#     hashed = ph.hash(password)
-#     print(hashed)
-
-
 def load_user_passwords(file_to_read, file_to_write):
-    ph = PasswordHasher()
-    file_r = open(file_to_read, 'r')
-    file_w = open(file_to_write, 'w')
-    for line in file_r.readlines():
-        tokens = line.replace('\n', '').split(' ')
-        # file_w.write('{} {}\n'.format(tokens[0], nacl.pwhash.str(tokens[1].encode('utf-8'))))
-        file_w.write('{} {}\n'.format(tokens[0], ph.hash(tokens[1].encode('utf-8'))))
-    file_r.close()
-    file_w.close()
+    argon = PasswordHasher()
 
-def compare_hashes(input_hash, pwd_to_hash):
+    read_file = open(file_to_read, 'r')
+    write_file = open(file_to_write, 'w')
+
+    for file_line in read_file.readlines():
+        split = file_line.replace('\n', '').split(' ')
+        write_file.write('{} {}\n'.format(split[0], argon.hash(split[1])))
+
+    read_file.close()
+    write_file.close()
+
+
+def argon_verify_by(input_hash, pwd_to_hash, argon):
     if not pwd_to_hash:
         return False
-    sha256 = nacl.hash.sha512(pwd_to_hash.encode('utf-8')).decode('utf-8')
-    if input_hash == sha256:
-        return True
-    sha512 = nacl.hash.sha512(pwd_to_hash.encode('utf-8')).decode('utf-8')
-    if input_hash == sha512:
-        return True
-    blake2b = nacl.hash.blake2b(pwd_to_hash.encode('utf-8')).decode('utf-8')
-    if input_hash == blake2b:
-        return True
-    return False
 
-def hash(input_hash, pwd_to_hash):
-    ph = PasswordHasher()
-    hashed = ph.hash(pwd_to_hash.encode('utf-8').decode('utf-8'))
-    print(hashed)
-    print(input_hash)
-    if input_hash == hashed:
-        return True
+    try:
+        if argon.verify(input_hash, pwd_to_hash):
+            return True
+    except argon2.exceptions.VerifyMismatchError:
+        return False
+
+    return False
 
 
 if __name__ == '__main__':
-    load_user_passwords('user-base.txt', 'user-hashes.txt')
-    file_bob = open('user-hashes.txt', 'r')
-    file_eve = open('common-passwords.txt', 'r')
+    argon_ph = PasswordHasher()
+
+    load_user_passwords('user-plain-text-password.txt', 'user-hashed-password.txt')
+    leaked_passwords = open('leaked-passwords.txt', 'r')
+    hashed_passwords = open('user-hashed-password.txt', 'r')
+
     common_passwords = []
-    for line in file_eve.readlines():
+
+    for line in leaked_passwords.readlines():
         common_passwords.append(line.replace('\n', ''))
-    file_eve.close()
-    for user_lines in file_bob.readlines():
+
+    leaked_passwords.close()
+
+    for user_lines in hashed_passwords.readlines():
         for pwd in common_passwords:
             tokens = user_lines.replace('\n', '').split(' ')
-            if hash(tokens[1], pwd):
-                print('User {} has password {}'.format(tokens[0], pwd))
-    file_bob.close()
+
+            if argon_verify_by(tokens[1], pwd, argon_ph):
+                print('Password leaked!!! Hackers revealed password: {} for User: {}'.format(pwd, tokens[0]))
+
+    hashed_passwords.close()
